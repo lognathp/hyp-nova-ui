8026a1dfc89782ec700fc26d538f44ded918dc4f
Aasif Ali <aasifali1088@gmail.com>
2025-04-21 16:18:06 +0530

    Onboard Chaitanya Food Court

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,30 +1,17 @@
 
 export const environment = {
     //baseUrl:'https://aardvark-notable-terminally.ngrok-free.app/api/v2',
-    //baseWsUrl:'ws://aardvark-notable-terminally.ngrok-free.app/api/v2/ws',  https://api.hyperapps.digital/api/v2/order?sortField=id&customerId_eq=100006
-    baseUrl: 'https://api.hyperapps.digital/api/v2',
-    baseWsUrl: 'ws://api.hyperapps.digital/api/v2/ws',
-    restId: '14490',
-    partnerId: 'ju8tCXSeun',
+    //baseWsUrl:'ws://aardvark-notable-terminally.ngrok-free.app/api/v2/ws',
+    baseUrl:'https://api.hyperapps.in/api/v2',
+    baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
+    restId:'15162',
+    partnerId:'ACUDTttX1H',
     production: false,
-    razorPayKey: 'rzp_test_dUt1jZJm3Whtxk',
-    title: 'Hyperapps',
-    contactRestaurant: "9642887733",
-    contactHyperapps: "8885026686",
-    flatDiscountpercentage: 25,
-    deliveryDiscount: 30,
-    itemdiscountValue: 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    razorPayKey:'rzp_test_dUt1jZJm3Whtxk',
+    title:'Chaitanya Food Court',
+    contactRestaurant:"9642887733",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 30,
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
