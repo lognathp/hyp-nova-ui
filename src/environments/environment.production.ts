d12e5c150de442270314a6c16ae68bcd648ad024
Aasif Ali <aasifali1088@gmail.com>
2025-05-02 12:28:14 +0530

    Onboard Shah Ghouse

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,12 +1,12 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'345121',
-    partnerId:'EoPZmWWGxr',
+    restId:'59654',
+    partnerId:'37xELG0Ki2',
     production: true,
-    razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
+    razorPayKey:'rzp_live_q2vhcACR4RokpK',
+    title:'Shah Ghouse',
+    contactRestaurant:"9700150828",
     contactHyperapps:"8885026686",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
@@ -24,4 +24,7 @@ export const environment = {
         applicable:false,
         offsetValue:199,
     }
+    flatDiscountpercentage : 0,
+    deliveryDiscount : 0,
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
