0572c5b9b443e2a6e8f101c4c87c183e272f6db0
Aasif Ali <aasifali1088@gmail.com>
2025-05-02 17:25:24 +0530

    Onboard Moghuls Paradiez

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'9334',
-    partnerId:'a8UuPbV7Er',
+    restId:'358659',
+    partnerId:'iFtgqbYtdY',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Yum Yum Tree',
