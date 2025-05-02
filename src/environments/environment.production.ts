a4e4e0118c673a82098433f424ef406012100c0e
Aasif Ali <aasifali1088@gmail.com>
2025-05-02 17:25:24 +0530

    Onboard Moghuls Paradiez

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,15 +1,15 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'131508',
-    partnerId:'rnCDvOSXZK',
+    restId:'358659',
+    partnerId:'iFtgqbYtdY',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Chaitanya Food Court',
-    contactRestaurant:"9182650986",
-    contactHyperapps:"8885026686",
-    flatDiscountpercentage : 0,
-    deliveryDiscount : 0,
+    title:'Moghuls Paradiez',
+    contactRestaurant:"9912306049",
+    contactHyperapps:"8885025585",
+    flatDiscountpercentage : 15,
+    deliveryDiscount : 30,
     itemdiscountValue : 0,
     itempackagingCharge: 10,  // Set Packing charge as applicable
     packingTaxPercentage:5,  // Packing charge tax in percentage
