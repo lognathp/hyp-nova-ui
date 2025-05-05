905ca319dea15c4a32bbc6646267eeae44ebda72
Aasif Ali <aasifali1088@gmail.com>
2025-05-05 21:02:16 +0530

    modiefied delivery discount

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -9,19 +9,6 @@ export const environment = {
     contactRestaurant:"7569263150",
     contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 30,
-    itemdiscountValue : 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    deliveryDiscount : 0,
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
