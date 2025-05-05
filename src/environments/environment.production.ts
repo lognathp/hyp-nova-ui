770062d45c9816500f7353ad1147793cafcb5740
Aasif Ali <aasifali1088@gmail.com>
2025-05-05 21:02:16 +0530

    modiefied delivery discount

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -5,23 +5,10 @@ export const environment = {
     partnerId:'rnCDvOSXZK',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"8885026686",
+    title:'Kanchi Cafe',
+    contactRestaurant:"7569263150",
+    contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
-    itemdiscountValue : 0,
-    itempackagingCharge: 0,  // Set Packing charge as applicable
-    packingTaxPercentage:0,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        // { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
