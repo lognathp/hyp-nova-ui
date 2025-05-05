ff367725f69eabfb3209e80f96c43d0f58977d64
Aasif Ali <aasifali1088@gmail.com>
2025-05-05 21:02:16 +0530

    modiefied delivery discount

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -9,7 +9,7 @@ export const environment = {
     contactRestaurant:"7569263150",
     contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 30,
+    deliveryDiscount : 0,
     itemdiscountValue : 0,
     itempackagingCharge: 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
