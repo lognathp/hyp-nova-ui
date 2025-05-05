0d47851887e9da6afb40baee70c360eae9dcf5ab
Aasif Ali <aasifali1088@gmail.com>
2025-05-05 13:39:09 +0530

    Onboard Satti Babu

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,27 +1,14 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'131508',
-    partnerId:'rnCDvOSXZK',
+    restId:'64340',
+    partnerId:'PKwxEF8exM',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Chaitanya Food Court',
-    contactRestaurant:"9182650986",
-    contactHyperapps:"8885026686",
-    flatDiscountpercentage : 0,
+    title:'Satti Babu Biryani',
+    contactRestaurant:"9492025490",
+    contactHyperapps:"8885025585",
+    flatDiscountpercentage : 25,
     deliveryDiscount : 0,
-    itemdiscountValue : 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,14 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'14490',
-    partnerId:'7ycrA3dec5',
+    restId:'64340',
+    partnerId:'PKwxEF8exM',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Hyperapps',
-    contactRestaurant:"9642887733",
-    contactHyperapps:"8885026686",
-    flatDiscountpercentage : 0,
+    title:'Satti Babu Biryani',
+    contactRestaurant:"9492025490",
+    contactHyperapps:"8885025585",
+    flatDiscountpercentage : 25,
     deliveryDiscount : 0,
-    itemdiscountValue : 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
