e0d38d8e0838943b16ea0a58b445fb5404c86c71
Aasif Ali <aasifali1088@gmail.com>
2025-05-06 13:27:25 +0530

    Onboard Mandar restaurant

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,27 +1,14 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'9334',
-    partnerId:'a8UuPbV7Er',
+    restId:'73680',
+    partnerId:'GtgBxICucn',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Yum Yum Tree',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
-    // flatDiscountpercentage : 25,
-    // deliveryDiscount : 25,
-    itemdiscountValue : 0,
-    itempackagingCharge: 0,  // Set Packing charge as applicable
-    packingTaxPercentage:0,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        // { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver:{
-        applicable:false,
-        offsetValue:199,
-    }
+    title:'Mandar Restaurant',
+    contactRestaurant:"9985544652",
+    contactHyperapps:"8885025585",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 0,
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
