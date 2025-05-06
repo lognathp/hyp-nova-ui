25a0d817fb38dd84fb5e159cedb961e2b442dc18
Aasif Ali <aasifali1088@gmail.com>
2025-05-06 15:16:11 +0530

    Onboard Babai Hotel

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,14 +1,14 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'131508',
-    partnerId:'rnCDvOSXZK',
+    restId:'689537',
+    partnerId:'glEoSsiMOp',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Kanchi Cafe',
-    contactRestaurant:"7569263150",
+    title:'Babai Hotel',
+    contactRestaurant:"9000358000",
     contactHyperapps:"8885025585",
-    flatDiscountpercentage : 25,
+    flatDiscountpercentage : 10,
     deliveryDiscount : 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
