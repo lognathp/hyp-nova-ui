58af1146559c04cb2c498f6a27d5657a441e20d7
Aasif Ali <aasifali1088@gmail.com>
2025-05-06 13:27:25 +0530

    Onboard Mandar restaurant

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,13 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'131508',
-    partnerId:'rnCDvOSXZK',
+    restId:'73680',
+    partnerId:'GtgBxICucn',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
+    title:'Mandar Restaurant',
+    contactRestaurant:"9985544652",
+    contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
