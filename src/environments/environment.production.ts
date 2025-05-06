4b8c9e5c1564b67508ed2bd606183ed085b6c7c3
Aasif Ali <aasifali1088@gmail.com>
2025-05-06 15:16:11 +0530

    Onboard Babai Hotel

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,15 +1,15 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'345121',
-    partnerId:'EoPZmWWGxr',
+    restId:'689537',
+    partnerId:'glEoSsiMOp',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
-    flatDiscountpercentage : 25,
-    deliveryDiscount : 25,
+    title:'Babai Hotel',
+    contactRestaurant:"9000358000",
+    contactHyperapps:"8885025585",
+    flatDiscountpercentage : 10,
+    deliveryDiscount : 0,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
