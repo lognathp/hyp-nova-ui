af6a6e242a741685161acc38de2d37235246bd21
Aasif Ali <aasifali1088@gmail.com>
2025-05-07 15:00:51 +0530

    slider content modiefied

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,14 @@
-
 export const environment = {
-    //baseUrl:'https://aardvark-notable-terminally.ngrok-free.app/api/v2',
-    //baseWsUrl:'ws://aardvark-notable-terminally.ngrok-free.app/api/v2/ws',
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
     restId:'59654',
     partnerId:'37xELG0Ki2',
-    production: false,
-    razorPayKey:'rzp_test_dUt1jZJm3Whtxk',
-    title:'Hyperapps',
-    contactRestaurant:"9642887733",
+    production: true,
+    razorPayKey:'rzp_live_q2vhcACR4RokpK',
+    title:'Shah Ghouse',
+    contactRestaurant:"8483884639",
     contactHyperapps:"8885026686",
-    flatDiscountpercentage : 25,
-    deliveryDiscount : 30,
-    itemdiscountValue : 0,
-    itempackagingCharge: 0,
+    flatDiscountpercentage : 0,
+    deliveryDiscount : 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
