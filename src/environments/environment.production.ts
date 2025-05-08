af61556928432ae00a8a7ccb4c2ae7339f2315ff
Sudhakar0724 <31535654+Sudhakar0724@users.noreply.github.com>
2025-05-08 14:21:11 +0530

    env config fix

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -10,5 +10,7 @@ export const environment = {
     contactHyperapps:"8885026686",
     flatDiscountpercentage : 15,
     deliveryDiscount : 0,
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
