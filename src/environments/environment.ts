f056133088914fcd4f6393daf3f458d5928e7323
Aasif Ali <aasifali1088@gmail.com>
2025-05-12 15:23:36 +0530

    Onboard Eruvaka Tiffins

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,15 @@
-
 export const environment = {
-    //baseUrl:'https://aardvark-notable-terminally.ngrok-free.app/api/v2',
-    //baseWsUrl:'ws://aardvark-notable-terminally.ngrok-free.app/api/v2/ws',  https://api.hyperapps.digital/api/v2/order?sortField=id&customerId_eq=100006
-    baseUrl:'https://api.hyperapps.digital/api/v2',
-    baseWsUrl:'ws://api.hyperapps.digital/api/v2/ws',
-    restId:'14490',
-    partnerId:'ju8tCXSeun',
-    production: false,
-    razorPayKey:'rzp_test_dUt1jZJm3Whtxk',
-    title:'Hyperapps',
-    contactRestaurant:"9642887733",
+    baseUrl:'https://api.hyperapps.in/api/v2',
+    baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
+    restId:'83286',
+    partnerId:'J7VpGrnYKN',
+    production: true,
+    razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
+    title:'Eruvaka Tiffins',
+    contactRestaurant:"9849122247",
     contactHyperapps:"8885026686",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 30,
+    deliveryDiscount : 0,
     itemdiscountValue : 0,
     itempackagingCharge: 0,
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
