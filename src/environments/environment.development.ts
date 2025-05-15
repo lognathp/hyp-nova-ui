d8e84929191b57fadb6b477ced02dbc5f9512675
Aasif Ali <aasifali1088@gmail.com>
2025-05-15 12:50:16 +0530

    added NON-POS outlet

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,24 +4,13 @@ export const environment = {
     restId: '14490',
     partnerId: 'ju8tCXSeun',
     production: false,
-    razorPayKey: 'rzp_test_dUt1jZJm3Whtxk',
-    title: 'Hyperapps',
-    contactRestaurant: "9642887733",
-    contactHyperapps: "8885026686",
-    flatDiscountpercentage: 25,
-    deliveryDiscount: 30,
-    itemdiscountValue: 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage: 5,  // Packing charge tax in percentage
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
-    deliveryOptions: [
-        // Un-comment to add options Comment to remove delivery options
-        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
-        // { label: 'Dine In', value: '2', index: 2 }
-    ],
-    deliveryWaiver: {
-        applicable: false,
-        offsetValue: 199,
-    }
+    razorPayKey:'rzp_test_dUt1jZJm3Whtxk',
+    title:'Hyperapps',
+    contactRestaurant:"9642887733",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 0,
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
 };
