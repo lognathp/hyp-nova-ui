719907e15c9384bee03e91397d1ac2f10c8c7255
Aasif Ali <aasifali1088@gmail.com>
2025-05-19 09:49:01 +0530

    Delivery options, packing charges, Free delivery on 199

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -74,7 +74,7 @@ export class CartComponent implements OnInit, AfterViewInit, DoCheck {
     unKnownError: boolean = false;
     restaurentClosed: boolean = false;
 
-    deliveryDiscount : any;
+    deliveryDiscount = environment.deliveryDiscount;
 
     orderOptions = environment.deliveryOptions;
     orderOptionsType = environment.deliveryOptions[0].value;
