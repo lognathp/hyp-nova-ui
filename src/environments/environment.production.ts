ff076c955f542d9e73c0a34fbf7820b666f9ad0d
Aasif Ali <aasifali1088@gmail.com>
2025-05-19 09:49:01 +0530

    Delivery options, packing charges, Free delivery on 199

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -23,7 +23,7 @@ export const environment = {
         // { label: 'Dine In', value: '2', index: 2 }
     ],
     deliveryWaiver:{
-        applicable:false,
+        applicable:true,
         offsetValue:199,
     }
 };
