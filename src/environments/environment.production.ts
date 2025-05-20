3fb48b586a530aaeeda6264ae669de43c5e86bfc
Aasif Ali <aasifali1088@gmail.com>
2025-05-20 17:06:18 +0530

    change the quote text

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -11,22 +11,18 @@ export const environment = {
     flatDiscountpercentage : 0,
     deliveryDiscount : 0,
     itemdiscountValue : 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
     deliveryWaiver:{
         applicable:false,
         offsetValue:199,
     }
-    flatDiscountpercentage : 0,
-    deliveryDiscount : 0,
-    itemdiscountValue : 0,
-    itempackagingCharge: 0,
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
+   
 };
