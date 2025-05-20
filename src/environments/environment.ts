b6d2ee68fd2f2f2c4d6218d0f5c285a1353b9874
Aasif Ali <aasifali1088@gmail.com>
2025-05-20 14:21:28 +0530

    Onboard Lobstar Kitchen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,23 @@
-
 export const environment = {
-    //baseUrl:'https://aardvark-notable-terminally.ngrok-free.app/api/v2',
-    //baseWsUrl:'ws://aardvark-notable-terminally.ngrok-free.app/api/v2/ws',  https://api.hyperapps.digital/api/v2/order?sortField=id&customerId_eq=100006
-    baseUrl: 'https://api.hyperapps.digital/api/v2',
-    baseWsUrl: 'ws://api.hyperapps.digital/api/v2/ws',
-    restId: '14490',
-    partnerId: 'ju8tCXSeun',
-    production: false,
-    razorPayKey: 'rzp_test_dUt1jZJm3Whtxk',
-    title: 'Hyperapps',
-    contactRestaurant: "9642887733",
-    contactHyperapps: "8885026686",
-    flatDiscountpercentage: 25,
-    deliveryDiscount: 30,
-    itemdiscountValue: 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage:5,  // Packing charge tax in percentage
+    baseUrl:'https://api.hyperapps.in/api/v2',
+    baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
+    restId:'88052',
+    partnerId:'BDw0t1Ocei',
+    production: true,
+    razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
+    title:'The Lobster Kitchen',
+    contactRestaurant:"9032414309",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 0,
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
     deliveryWaiver:{
