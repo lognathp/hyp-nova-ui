3fdb7b6950373373728362fa8de1982130bad0de
Aasif Ali <aasifali1088@gmail.com>
2025-05-20 11:46:03 +0530

    Onboard Punjabi Dhaba

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -9,7 +9,7 @@ export const environment = {
     contactRestaurant: "9642887733",
     contactHyperapps: "8885026686",
     flatDiscountpercentage: 25,
-    deliveryDiscount: 30,
+    deliveryDiscount: 0,
     itemdiscountValue: 0,
     itempackagingCharge: 10,  // Set Packing charge as applicable
     packingTaxPercentage: 5,  // Packing charge tax in percentage
