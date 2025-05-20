b7fba1be172df01afa89212f0178328ae27ac5cd
Aasif Ali <aasifali1088@gmail.com>
2025-05-20 16:56:09 +0530

    change the quote text

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,14 +1,27 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'345121',
-    partnerId:'EoPZmWWGxr',
+    restId:'135348',
+    partnerId:'Lbqg9o4ib2',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
+    title:'Wacky-wok',
+    contactRestaurant:"6301100915",
+    contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
+    deliveryOptions: [
+        // Un-comment to add options Comment to remove delivery options
+        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
+        // { label: 'Dine In', value: '2', index: 2 }
+    ],
+    deliveryWaiver:{
+        applicable:false,
+        offsetValue:199,
+    }
 };
