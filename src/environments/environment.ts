aac488156c67b97d6a86957eec28c7796a22acf5
Aasif Ali <aasifali1088@gmail.com>
2025-05-22 15:24:23 +0530

    Onboard Seenu C/O Bezwada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'9334',
-    partnerId:'a8UuPbV7Er',
+    restId:'376937',
+    partnerId:'sXFZ90j99L',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Yum Yum Tree',
-    contactRestaurant:"8179644836",
+    title:'Seenu C/O Bezawada',
+    contactRestaurant:"9550888641",
     contactHyperapps:"7801057583",
     // flatDiscountpercentage : 25,
     // deliveryDiscount : 25,
@@ -17,6 +17,7 @@ export const environment = {
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
