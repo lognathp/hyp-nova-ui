87256db41685e47a3c5e3bb0e72cd903e5096cee
Aasif Ali <aasifali1088@gmail.com>
2025-05-23 16:12:34 +0530

    Onboard Ankapur

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,15 +1,15 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'206207',
-    partnerId:'3Ze9YGDJpw',
+    restId:'72571',
+    partnerId:'Z6kiCVKf6x',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Saharsh Udupi Grand',
-    contactRestaurant:"9000901009",
-    contactHyperapps:"7801057583",
-    // flatDiscountpercentage : 25,
-    // deliveryDiscount : 25,
+    title:'Ankapur Village',
+    contactRestaurant:"9642883377",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 30,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,18 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'206207',
-    partnerId:'3Ze9YGDJpw',
+    restId:'72571',
+    partnerId:'Z6kiCVKf6x',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Saharsh Udupi Grand',
-    contactRestaurant:"9000901009",
-    contactHyperapps:"7801057583",
-    // flatDiscountpercentage : 25,
-    // deliveryDiscount : 25,
+    title:'Ankapur Village',
+    contactRestaurant:"9642883377",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 30,
     itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
@@ -19,6 +21,7 @@ export const environment = {
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
