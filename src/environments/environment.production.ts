5513330327d1ea5955815fb1bc277ed6eba47ba9
Aasif Ali <aasifali1088@gmail.com>
2025-05-26 13:10:24 +0530

    hide, reorder and download invoice button from order history

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -9,6 +9,19 @@ export const environment = {
     contactRestaurant:"9985544652",
     contactHyperapps:"8885025585",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 0,
-    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw'
+    deliveryDiscount : 30,
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
+    googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
+    deliveryOptions: [
+        // Un-comment to add options Comment to remove delivery options
+        { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
+        // { label: 'Dine In', value: '2', index: 2 }
+    ],
+    deliveryWaiver:{
+        applicable:false,
+        offsetValue:199,
+    }
 };
