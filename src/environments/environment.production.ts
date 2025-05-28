47f3b15d85812394ad4a85bdc3b87a3738b120ed
Aasif Ali <aasifali1088@gmail.com>
2025-05-28 12:21:11 +0530

    delivery share updated

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -9,7 +9,7 @@ export const environment = {
     contactRestaurant:"7799997883",
     contactHyperapps:"8885026686",
     flatDiscountpercentage : 15,
-    deliveryDiscount : 0,
+    deliveryDiscount : 30,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
