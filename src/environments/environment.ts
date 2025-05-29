ebbe46866da9c13794e43ca9111d97fdebdcf898
Aasif Ali <aasifali1088@gmail.com>
2025-05-29 10:58:23 +0530

    Onboard United Kitchen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'206207',
-    partnerId:'3Ze9YGDJpw',
+    restId:'71723',
+    partnerId:'5BBiPSqr3m',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Saharsh Udupi Grand',
@@ -13,12 +13,15 @@ export const environment = {
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     WHATSAPP_API_URL: 'https://graph.facebook.com/v22.0/112915418570784/messages',
     WHATSAPP_TOKEN: 'EAAxcoXYnGfgBO1qwf4g9ZBCatP9PO8ZCTZA8ZBR50gLBXeF0Kd9rlP6uT4hwvyzuOQRnPF6iRH9r8e7bRMGqCHgnEce87AF3duKFvzwBjoZCZBJMHilifHqS7DOHiGVU28pnYmPhe6uHvB9MmsuAARoV1ZBm6E2SyCtuMGojskokRZAZCKCw4ZB3gIlufSVEKBfQJhEAZDZD',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
