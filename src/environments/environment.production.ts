38d82fd7bc4452cfd2e04a3e0435de889a1bb8f5
Aasif Ali <aasifali1088@gmail.com>
2025-05-30 15:52:18 +0530

    Onboard Skandas Kitchen

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,22 +1,25 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'345121',
-    partnerId:'EoPZmWWGxr',
+    restId:'379245',
+    partnerId:'qPGxD4n4OH',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
+    title:'Skandas Kitchen',
+    contactRestaurant:"8639105939",
+    contactHyperapps:"8885026686",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
     itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
