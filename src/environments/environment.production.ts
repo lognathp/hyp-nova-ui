d2c2f30044d7f3183e6f42d0d18140d9494fc0f0
Aasif Ali <aasifali1088@gmail.com>
2025-05-30 15:30:29 +0530

    Onboard Hi tech bawarchi

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,22 +1,25 @@
 export const environment = {
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
-    restId:'9334',
-    partnerId:'a8UuPbV7Er',
+    restId:'115090',
+    partnerId:'1v74386prp',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Yum Yum Tree',
-    contactRestaurant:"8179644836",
-    contactHyperapps:"7801057583",
-    // flatDiscountpercentage : 25,
-    // deliveryDiscount : 25,
+    title:'Hi Tech Bawarchi Food Court',
+    contactRestaurant:"9618291465",
+    contactHyperapps:"8885026686",
+    flatDiscountpercentage : 25,
+    deliveryDiscount : 0,
     itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
