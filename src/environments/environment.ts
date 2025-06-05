56e6c811c15b933cb2d5c8f67d74315aa8b84a52
Aasif Ali <aasifali1088@gmail.com>
2025-06-05 18:46:07 +0530

    Onboard Shubham Restaurants

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
-    baseUrl:'https://api.hyperapps.digital/api/v2',
-    baseWsUrl:'wss://api.hyperapps.digital/api/v2/ws',
-    restId:'345121',
-    partnerId:'ju8tCXSeun',
+    baseUrl:'https://api.hyperapps.in/api/v2',
+    baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
+    restId:'50572',
+    partnerId:'INhHv4LZfc',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
+    title:'Hotel Shubham Celebrations',
     contactRestaurant:"8179644836",
     contactHyperapps:"8885026686",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 0,
+    deliveryDiscount : 30,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
