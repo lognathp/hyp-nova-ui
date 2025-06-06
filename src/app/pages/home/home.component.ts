904a570b23f1e0d7606229107402a24a28c1b5f6
Aasif Ali <aasifali1088@gmail.com>
2025-06-06 15:43:50 +0530

    Onboard Yummy Bee

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -28,7 +28,7 @@ export class HomeComponent {
     let domain = new URL(url).origin;
     console.log('isDevMode', isDevMode());
     if (isDevMode()) {
-      domain = "https://yumyumtree.hyperapps.in"
+      domain = "https://yummybee.hyperapps.in"
     }
     this.apiService.getMethod(`/partner?domain_eq=${domain}`).subscribe({
       next: (response) => {
