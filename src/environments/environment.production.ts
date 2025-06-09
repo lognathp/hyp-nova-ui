0ceabddddb7a3df4cd75c73ea83eb096b9eae48d
Aasif Ali <aasifali1088@gmail.com>
2025-06-09 14:06:24 +0530

    contact number modiefied

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -7,7 +7,7 @@ export const environment = {
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Ideal Kitchen',
     contactRestaurant:"9642887733",
-    contactHyperapps:"8885026686",
+    contactHyperapps:"7801057583",
     flatDiscountpercentage : 25,
     deliveryDiscount : 30,
     itemdiscountValue : 0,
