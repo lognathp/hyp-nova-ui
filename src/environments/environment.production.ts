5a55c4fe3095b435a7fd464829d45efa89a105e9
Aasif Ali <aasifali1088@gmail.com>
2025-06-10 15:03:11 +0530

    rebased

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -7,9 +7,8 @@ export const environment = {
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Eruvaka Tiffins',
     contactRestaurant:"9849122247",
-    contactHyperapps:"8885026686",
+    contactHyperapps:"7801057583",
     flatDiscountpercentage : 25,
-    deliveryDiscount : 0,
     deliveryDiscount : 0,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
