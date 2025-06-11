2478c56dcebb62ba01e6f8f4c67082e6f4d1de4c
Aasif Ali <aasifali1088@gmail.com>
2025-06-11 18:28:34 +0530

    price level discount modiefied

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,8 @@ export const environment = {
     contactHyperapps:"7801057583",
     // flatDiscountpercentage : 25,
     // deliveryDiscount : 25,
+    flatDiscountpercentage : 0,
+    deliveryDiscount : 50,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
