fef61c20633a6fe19f2a18cacdc7bcd99ab02b87
Aasif Ali <aasifali1088@gmail.com>
2025-06-12 16:08:32 +0530

    rebased

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
-    baseUrl:'https://api.hyperapps.digital/api/v2',
-    baseWsUrl:'wss://api.hyperapps.digital/api/v2/ws',
-    restId:'345121',
-    partnerId:'ju8tCXSeun',
+    baseUrl:'https://api.hyperapps.in/api/v2',
+    baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
+    restId:'146034',
+    partnerId:'1ggC64kTy3',
     production: true,
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'The Nawaabs Restaurant',
-    contactRestaurant:"8179644836",
+    title:'Palle Vindu',
+    contactRestaurant:"7799997883",
     contactHyperapps:"7801057583",
-    flatDiscountpercentage : 25,
-    deliveryDiscount : 25,
+    flatDiscountpercentage : 15,
+    deliveryDiscount : 0,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
