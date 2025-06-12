df2a12cab49f1551dab7922d9c5750f2709bb72f
Aasif Ali <aasifali1088@gmail.com>
2025-06-12 13:20:07 +0530

    rebased

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -7,7 +7,7 @@ export const environment = {
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Sree Harsha Delux Mess',
     contactRestaurant:"7799810105",
-    contactHyperapps:"8885026686",
+    contactHyperapps:"7801057583",
     flatDiscountpercentage : 25,
     deliveryDiscount : 0,
     itemdiscountValue : 0,
