5284b7885598f9dfb753420a1f18e62ce23520d6
Aasif Ali <aasifali1088@gmail.com>
2025-06-13 18:23:23 +0530

    removed one outlet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
-    baseUrl:'https://api.hyperapps.digital/api/v2',
-    baseWsUrl:'wss://api.hyperapps.digital/api/v2/ws',
-    restId:'345121',
-    partnerId:'ju8tCXSeun',
-    production: true,
-    razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
-    title:'Saharsh Udupi Grand',
-    contactRestaurant:"9000901009",
-    contactHyperapps:"7801057583",
-    flatDiscountpercentage : 25,
-    deliveryDiscount : 25,
-=======
     baseUrl:'https://api.hyperapps.in/api/v2',
     baseWsUrl:'wss://api.hyperapps.in/api/v2/ws',
     restId:'40401',
@@ -20,15 +7,9 @@ export const environment = {
     razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
     title:'Sri Kanya',
     contactRestaurant:"9963356633",
-    contactHyperapps:"8885026686",
-<<<<<<< HEAD
-    flatDiscountpercentage : 25,
-    deliveryDiscount : 0,
->>>>>>> 38ee6fc (hide, reorder and download invoice button from order history)
-=======
+    contactHyperapps:"7801057583",
     flatDiscountpercentage : 15,
     deliveryDiscount : 20,
->>>>>>> 9ae74b6 (modiefied config)
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
