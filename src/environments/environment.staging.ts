a4ffc6356afd3deae3f4840d4b4ca42f8b057386
Aasif Ali <aasifali1088@gmail.com>
2025-07-04 10:09:50 +0530

    added outlet

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,29 +1,29 @@
 export const environment = {
-    baseUrl: 'https://api.hyperapps.digital/api/v2',
-    baseWsUrl: 'wss://api.hyperapps.digital/api/v2/ws',
-    restId: '14490',
-    partnerId: 'ju8tCXSeun',
-    production: false,
-    razorPayKey: 'rzp_test_dUt1jZJm3Whtxk',
-    title: 'Hyperapps',
-    contactRestaurant: "9642887733",
-    contactHyperapps: "8885026686",
-    flatDiscountpercentage: 25,
-    deliveryDiscount: 30,
-    itemdiscountValue: 0,
-    itempackagingCharge: 10,  // Set Packing charge as applicable
-    packingTaxPercentage: 5,  // Packing charge tax in percentage
+   baseUrl: 'https://api.hyperapps.cloud/api/v2',
+    baseWsUrl: 'wss://api.hyperapps.cloud/api/v2/ws',
+    restId:'24601',
+    partnerId:'2bX96SzDSE',
+    production: true,
+    razorPayKey:'rzp_live_oUdd0v4WZIGGRP',
+    title:'Priya Darshini',
+    contactRestaurant:"9700313687",
+    contactHyperapps:"7801057583",
+    flatDiscountpercentage : 0,
+    deliveryDiscount : 25,
+    itemdiscountValue : 0,
+    itempackagingCharge: 0,  // Set Packing charge as applicable
+    packingTaxPercentage:0,  // Packing charge tax in percentage
     googleMapApiKey: 'AIzaSyDROVRwoyWdPPqvrzrA947dxZ-9F0HgRWw',
     WHATSAPP_API_URL: 'https://graph.facebook.com/v22.0/112915418570784/messages',
     WHATSAPP_TOKEN: 'EAAxcoXYnGfgBO1qwf4g9ZBCatP9PO8ZCTZA8ZBR50gLBXeF0Kd9rlP6uT4hwvyzuOQRnPF6iRH9r8e7bRMGqCHgnEce87AF3duKFvzwBjoZCZBJMHilifHqS7DOHiGVU28pnYmPhe6uHvB9MmsuAARoV1ZBm6E2SyCtuMGojskokRZAZCKCw4ZB3gIlufSVEKBfQJhEAZDZD',
     deliveryOptions: [
         // Un-comment to add options Comment to remove delivery options
         { label: 'Delivery', value: '1', index: 0 },   // Default type dont-touch
-        { label: 'Take Away', value: '3', index: 1 },
+        // { label: 'Take Away', value: '3', index: 1 },
         // { label: 'Dine In', value: '2', index: 2 }
     ],
-    deliveryWaiver: {
-        applicable: false,
-        offsetValue: 199,
+    deliveryWaiver:{
+        applicable:false,
+        offsetValue:199,
     }
 };
