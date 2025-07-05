4703bc6c635c40a285c15748065fb35a61a2139a
Aasif Ali <aasifali1088@gmail.com>
2025-07-05 17:13:49 +0530

    modiefied price strike and discount

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     title:'Priya Darshini',
     contactRestaurant:"9700313687",
     contactHyperapps:"7801057583",
-    flatDiscountpercentage : 0,
+    flatDiscountpercentage : 10,
     deliveryDiscount : 25,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
