c4d55065db9e97b7b5da442be697901a6f8325a5
Aasif Ali <aasifali1088@gmail.com>
2025-07-07 11:32:29 +0530

    rebased with main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
     title:'Shah Ghouse',
     contactRestaurant:"8483884639",
     contactHyperapps:"7801057583",
-    flatDiscountpercentage : 0,
-    deliveryDiscount : 45,
+    // flatDiscountpercentage : 0,
+    // deliveryDiscount : 45,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
