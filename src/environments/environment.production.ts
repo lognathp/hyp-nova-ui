0e1842e0e6216171bf8b518fcbf6d74615702b89
Aasif Ali <aasifali1088@gmail.com>
2025-07-18 16:01:44 +0530

    rebased with main

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -8,8 +8,8 @@ export const environment = {
     title:'Mandar Restaurant',
     contactRestaurant:"9985544652",
     contactHyperapps:"7801057583",
-    flatDiscountpercentage : 10,
-    deliveryDiscount : 20,
+    // flatDiscountpercentage : 10,
+    // deliveryDiscount : 20,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
