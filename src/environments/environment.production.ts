a70f2dac5e933dfd841da3549d42d34315b74742
Aasif Ali <aasifali1088@gmail.com>
2025-07-24 10:27:35 +0530

    rebased with main

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -8,8 +8,8 @@ export const environment = {
     title:'G Pulla Reddy',
     contactRestaurant:"7416095959",
     contactHyperapps:"7801057583",
-    flatDiscountpercentage : 0,
-    deliveryDiscount : 50,
+    // flatDiscountpercentage : 0,
+    // deliveryDiscount : 50,
     itemdiscountValue : 0,
     itempackagingCharge: 0,  // Set Packing charge as applicable
     packingTaxPercentage:0,  // Packing charge tax in percentage
